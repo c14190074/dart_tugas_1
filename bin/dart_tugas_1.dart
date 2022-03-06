3a4b6ee4bceb2d00f116da6a417eee655884b45c
c14190074 <c14190074@john.petra.ac.id>
2022-03-06 12:17:51 +0700

    Membuat pola diamond

--- bin/dart_tugas_1.dart
+++ bin/dart_tugas_1.dart
@@ -9,7 +9,57 @@ void main(List<String> arguments) {
     // genap
     print("Genap");
   } else {
-    // ganjil
-    print("Ganjil");
+    // ganjil => diamond
+    int checkMarker = 0;
+    int marker = n;
+    n = (n ~/ 2) + 1;
+
+    for (int i = 1; i <= n; i++) {
+      checkMarker = 0;
+      for (int j = 1; j <= (n * 2 - 1); j++) {
+        if ((n - j).abs() < i) {
+          checkMarker = printMarker(checkMarker);
+
+          if (checkMarker == 1) {
+            stdout.write(i);
+            marker = i;
+          } else {
+            stdout.write(" ");
+          }
+        } else {
+          stdout.write("*");
+        }
+      }
+      print("");
+    }
+
+    n -= 1;
+    marker += 1;
+    for (int i = n; i >= 1; i--) {
+      checkMarker = 0;
+      for (int j = 0; j <= (n * 2); j++) {
+        if ((n - j).abs() < i) {
+          checkMarker = printMarker(checkMarker);
+
+          if (checkMarker == 1) {
+            stdout.write(marker);
+          } else {
+            stdout.write(" ");
+          }
+        } else {
+          stdout.write("*");
+        }
+      }
+      marker += 1;
+      print("");
+    }
+  } // end of else
+}
+
+printMarker(int x) {
+  if (x == 0) {
+    return 1;
+  } else {
+    return 0;
   }
 }
